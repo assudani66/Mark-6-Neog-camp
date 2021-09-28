a2d3698128676cfd47df2a7a3518fb0652e5f33a
Phantomcat20 <63794264+assudani66@users.noreply.github.com>
2021-09-28 20:52:39 +0530

    Fixed Link

--- Minion Translator/app.js
+++ Minion Translator/app.js
@@ -1,6 +1,6 @@
 // minion server URL
 
-var ServerURL = "https://api.funtranslations.com/translate/minion.json?text=Hello%2C%20I%20am%20hungry%21"
+var ServerURL = "https://api.funtranslations.com/translate/minion.json"
 
 //queryselecting terms
 
@@ -23,7 +23,9 @@ btntranslate.addEventListener("click" , function clickresposer(){
     
     fetch(getTranslationURL(text))   
     .then(response => response.json())
-    .then(json => {txtoutput.innerText = json.contents.translated})
-    .then(console.log(txtoutput.innerText))
+    .then(json => { txtoutput.value = json.contents.translated;
+        console.log(json.contents.translated)
+    })
+    .then(console.log(txtoutput.value))
     .catch(errorhandler)
 })
